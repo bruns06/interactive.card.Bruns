f0ea8028358d4e590d12974265063ac8baa2cca9
Paolo Bruna <bruns2506@gmail.com>
2023-02-09 00:44:06 +0100

    aldo moro mio padre

---
+++ script/main.js
@@ -0,0 +1,21 @@
+
+let dNome = () => {
+    document.getElementById('dNome').innerHTML = document.getElementById('name').value;
+}
+
+let getNumber = () =>{
+    document.getElementById('dCognome').innerHTML = document.getElementById('number').value;
+} 
+
+let getMonth = () =>{
+    document.getElementById('dMese').innerHTML = document.getElementById('month').value;
+} 
+
+let getYear = () =>{
+    document.getElementById('dAnno').innerHTML = document.getElementById('yeare').value;
+}
+let getCVC = () =>{
+    document.getElementById('dCVC').innerHTML = document.getElementById('cvc').value;
+}
+
+
